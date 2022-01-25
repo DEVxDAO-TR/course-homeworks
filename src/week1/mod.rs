7612daeb238397eaca7cfc28cd3808897ada0fc0
Emin Fedar <eminfedar@gmail.com>
2022-01-25 14:19:30 +0300

    Added week1 and week2 tests

---
+++ src/week1/mod.rs
@@ -0,0 +1,9 @@
+pub fn hello(name:&str) -> String {
+
+}
+pub fn make_it_double(num:i32) -> i32 {
+
+}
+pub fn multiply_pi(num:f64) -> f64 {
+    
+}
---
+++ src/week2/mod.rs
@@ -0,0 +1,21 @@
+// Homework - 1
+pub fn to_letter_grade(num:u8) -> String {
+
+}
+
+// Homework - 2
+pub enum LogLevel {
+    Msg,
+    Warn,
+    Err
+}
+pub fn log(level:LogLevel, msg:&str) -> String {
+
+}
+
+// Homework - 3
+pub enum Gender {
+    Male,
+    Female
+}
+pub struct Person {name:String, age:u8, gender:Gender}
