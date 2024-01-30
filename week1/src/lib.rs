95fbb3bc3edc08bd16b03d6701a1e7397e5b77f2
Emin Fedar <eminfedar@gmail.com>
2024-01-30 17:31:28 +0300

    kış kampı için

--- week1/src/lib.rs
+++ week1/src/lib.rs
@@ -1,11 +1,1 @@
-pub fn hello(name:&str) -> String {
-    String::from("") // Write Your Code Here
-}
-
-pub fn make_it_double(num:i32) -> i32 {
-    0 // Write Your Code Here
-}
-
-pub fn multiply_pi(num:f64) -> f64 {
-    0.0 // Write Your Code Here
-}
+pub mod eminfedar;
