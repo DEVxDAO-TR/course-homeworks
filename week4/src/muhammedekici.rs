df4d8ac773204ee8adbdc687011349df0e3fe7ae
ekcmuhammed08 <ekcmuhammed08@gmail.com>
2024-01-31 15:35:31 +0300

    week 4 update

---
+++ week4/src/muhammedekici.rs
@@ -0,0 +1,78 @@
+pub fn even_list(v:Vec<i32>) -> Vec<i32>{
+    v.into_iter().filter(|x| x%2 == 0).collect()
+}
+
+pub fn double_list(v:Vec<i32>) -> Vec<i32>{
+    v.into_iter().map(|x| x*2).collect()
+}
+
+pub struct MultiFibonacci {
+    first:u32, 
+    second:u32
+}
+
+impl MultiFibonacci {
+    fn new() -> Self {
+        Self {first:1, second:2}
+    }
+}
+
+impl Iterator for MultiFibonacci {
+    type Item = u32;
+
+    fn next(&mut self)-> Option<Self::Item>{
+        let sonuc:u32 = self.first * self.second; 
+        if sonuc <=8192 {
+            self.first = self.second;
+            self.second = sonuc;
+            Some(sonuc)
+        }
+        else{
+            None
+        }
+    }
+    
+}
+
+
+
+#[cfg(test)]
+mod week4_tests {
+    use super::*;
+
+    #[test]
+    fn even_list() {
+        let v1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let v1 = super::even_list(v1);
+
+        let v2 = vec![1, 3, 5, 7];
+        let v2 = super::even_list(v2);
+
+        assert_eq!(v1, vec![2, 4, 6, 8, 10]);
+        assert_eq!(v2, vec![]);
+    }
+
+    #[test]
+    fn double_list() {
+        let v1 = vec![1, 2, 3, 4, 5];
+        let v1 = super::double_list(v1);
+
+        let v2 = vec![0, 5, 10, 15];
+        let v2 = super::double_list(v2);
+
+        assert_eq!(v1, vec![2, 4, 6, 8, 10]);
+        assert_eq!(v2, vec![0, 10, 20, 30]);
+    }
+
+    #[test]
+    fn multiply_fibonacci() {
+        let mut mf_iter = MultiFibonacci::new();
+
+        assert_eq!(mf_iter.next().unwrap(), 2);
+        assert_eq!(mf_iter.next().unwrap(), 4);
+        assert_eq!(mf_iter.next().unwrap(), 8);
+        assert_eq!(mf_iter.next().unwrap(), 32);
+        assert_eq!(mf_iter.next().unwrap(), 256);
+        assert_eq!(mf_iter.next().unwrap(), 8192);
+    }
+}
