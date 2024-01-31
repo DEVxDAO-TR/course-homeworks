4193e8efda2c356a75d00836bba6532a82cbcd42
Emin Fedar <eminfedar@gmail.com>
2024-01-31 12:09:53 +0300

    week3,4,5,6 new template added

--- week6/src/lib.rs
+++ week6/src/lib.rs
@@ -1,9 +1,46 @@
-use async_std::task;
-use async_std::io;
-use async_std::net::{TcpListener, TcpStream};
+automod::dir!(pub "src/");
 
+// Test Templates
+#[cfg(test)]
+#[cfg(target_os = "windows")] // burayı kendi kodunuzda silin
+#[cfg(test)]
+mod week6_tests {
+    use week6;
 
-pub fn start_server() -> io::Result<()> {
+    use std::io::{self, Read, Write};
+    use std::net::{Shutdown, TcpStream};
+    use std::thread;
+    use std::time::Duration;
 
-    Ok(())
+    fn client() -> io::Result<String> {
+        let mut stream = TcpStream::connect("127.0.0.1:22222")?;
+        thread::sleep(Duration::from_secs(1));
+
+        let mut request = String::from("Merhaba");
+        stream.write_all(request.as_bytes())?;
+
+        thread::sleep(Duration::from_secs(1));
+
+        let mut response = [0u8; 128];
+        let n = stream.read(&mut response)?;
+
+        stream.shutdown(Shutdown::Both).unwrap();
+
+        request.push_str(std::str::from_utf8(&response[0..n]).unwrap());
+
+        Ok(request)
+    }
+
+    #[test]
+    fn test_server() {
+        thread::spawn(|| week6::start_server().unwrap());
+
+        let client1 = thread::spawn(client);
+        let client2 = thread::spawn(client);
+
+        let c1_response = client1.join().unwrap().unwrap();
+        let c2_response = client2.join().unwrap().unwrap();
+
+        assert_eq!(c1_response, c2_response);
+    }
 }
