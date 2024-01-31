dbd22c06839a78409eafd1fd5db8536124412033
Arda Altıntaş <73823004+ardanyan@users.noreply.github.com>
2024-01-31 16:03:41 +0300

    week4 again
    here we go again

--- week4/src/arda_altintas.rs
+++ week4/src/arda_altintas.rs
@@ -1,24 +1,10 @@
 pub fn even_list(v : Vec<i32>) ->Vec<i32>{
-
-    let mut even_list = Vec::new();
-
-    for i in v{
-        if i % 2 == 0{
-            even_list.push(i);
-        }
-    }
-    even_list
+    v.into_iter().filter(|&x| x % 2 == 0).collect()
 }
 
 
 pub fn double_list(v : Vec<i32>) ->Vec<i32>{
-
-    let mut double_list = Vec::new();
-
-    for i in v{
-        double_list.push(i*2);
-    }
-    double_list
+    v.into_iter().map(|x| x * 2).collect()
 }
 
 pub struct MultiFibonacci {
@@ -49,22 +35,8 @@ impl Iterator for MultiFibonacci {
 
 
 pub fn multiply_fibonacci() -> Vec<i32>{
-
-    let mut fibonacci = Vec::new();
-    fibonacci.push(1);
-    fibonacci.push(1);
-
-    let mut i = 2;//starts from second element [0] = 1, [1] = 2
-    while i < 10{
-        fibonacci.push(fibonacci[i-1] + fibonacci[i-2]);
-        i += 1;
-    }
-
-    let mut multiply_fibonacci = Vec::new();
-    for i in fibonacci{
-        multiply_fibonacci.push(i*2);
-    }
-    multiply_fibonacci
+    let mut mf_iter = MultiFibonacci::new();
+    mf_iter.collect()
 }
 
 
